ffc2685d528a0bdc295e32b518044102991aac53
p-ninja <lovewolf1234@proton.me>
2019-11-24 18:38:43 +0100

    improved delete command and removed typing status

--- riddle_bot.py
+++ riddle_bot.py
@@ -96,48 +96,49 @@ class Bot(Client):
                     await message.channel.send("You are not authorized to use this command!")
                     return
 
-                async with message.channel.typing():
-                    level_id = self.get_max_level_id() + 1
-                    await message.channel.send(f"Creating Level {level_id}")
+                level_id = self.get_max_level_id() + 1
+                await message.channel.send(f"Creating Level {level_id}")
 
-                    role: Role = await self.guild.create_role(name=role_name(level_id), hoist=True)
+                role: Role = await self.guild.create_role(name=role_name(level_id), hoist=True)
 
-                    await self.sort_roles()
+                await self.sort_roles()
 
-                    level_channel: TextChannel = await self.level_category.create_text_channel(
-                        level_name(level_id),
-                        overwrites={
-                            self.guild.default_role: PermissionOverwrite(read_messages=False),
-                            role: PermissionOverwrite(read_messages=True, send_messages=False),
-                        },
-                    )
-                    solution_channel: TextChannel = await self.solution_category.create_text_channel(
-                        solution_name(level_id),
-                        overwrites={
-                            self.guild.default_role: PermissionOverwrite(read_messages=False),
-                            self.guild.me: PermissionOverwrite(read_messages=True),
-                        },
-                    )
-                    await message.channel.send(
-                        f"Level {level_id} has been created.\n"
-                        f"Level channel: {level_channel.mention}\n"
-                        f"Solution channel: {solution_channel.mention}\n"
-                        f"Role: {role.mention}"
-                    )
+                level_channel: TextChannel = await self.level_category.create_text_channel(
+                    level_name(level_id),
+                    overwrites={
+                        self.guild.default_role: PermissionOverwrite(read_messages=False),
+                        role: PermissionOverwrite(read_messages=True, send_messages=False),
+                    },
+                )
+                solution_channel: TextChannel = await self.solution_category.create_text_channel(
+                    solution_name(level_id),
+                    overwrites={
+                        self.guild.default_role: PermissionOverwrite(read_messages=False),
+                        self.guild.me: PermissionOverwrite(read_messages=True),
+                    },
+                )
+                await message.channel.send(
+                    f"Level {level_id} has been created.\n"
+                    f"Level channel: {level_channel.mention}\n"
+                    f"Solution channel: {solution_channel.mention}\n"
+                    f"Role: {role.mention}"
+                )
             elif cmd == "delete":
                 if not await self.is_authorized(message.author):
                     await message.channel.send("You are not authorized to use this command!")
                     return
 
                 if not args:
-                    await message.channel.send("usage: $delete <level-id>")
+                    await message.channel.send("usage: $delete <level-id> [<level-id>]")
                     return
                 else:
-                    if not args[0].isnumeric():
+                    if (not args[0].isnumeric()) or (len(args) > 1 and not args[1].isnumeric()):
                         await message.channel.send("Level ID has to be numeric!")
                         return
-                    level_id = int(args[0])
-                async with message.channel.typing():
+                    from_level_id = int(args[0])
+                    to_level_id = int(args[1]) if len(args) > 1 else from_level_id
+
+                for level_id in range(from_level_id, to_level_id + 1):
                     level_channel, solution_channel, role = self.get_level(level_id)
                     existed = False
                     if level_channel:
@@ -150,18 +151,21 @@ class Bot(Client):
                         await role.delete()
                         existed = True
 
-                    for level in self.get_levels():
-                        if level <= level_id:
-                            continue
-                        level_channel, solution_channel, role = self.get_level(level)
-                        await level_channel.edit(name=level_name(level - 1))
-                        await solution_channel.edit(name=solution_name(level - 1))
-                        await role.edit(name=role_name(level - 1))
-
                     if existed:
                         await message.channel.send(f"Level {level_id} has been deleted")
                     else:
                         await message.channel.send(f"Level {level_id} does not exist")
+                for level in self.get_levels():
+                    if level <= to_level_id:
+                        continue
+                    level_channel, solution_channel, role = self.get_level(level)
+                    level_count = to_level_id - from_level_id + 1
+                    await level_channel.edit(name=level_name(level - level_count))
+                    await solution_channel.edit(name=solution_name(level - level_count))
+                    await role.edit(name=role_name(level - level_count))
+
+                await message.channel.send("Done")
+
             elif cmd == "sort":
                 if not await self.is_authorized(message.author):
                     await message.channel.send("You are not authorized to use this command!")
